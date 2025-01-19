410b22f29b7eabe63ef77c7486abb3c3773270d4
BrayanG877 <gordillomartinezbrayan@gmail.com>
2025-01-19 15:10:47 -0500

    Update buscar.py

--- buscar.py
+++ buscar.py
@@ -1,10 +1,13 @@
 def Buscar_producto(inventario, Nombre_producto):
     if Nombre_producto in inventario:
         Datos = inventario[Nombre_producto]
-        print(f"\nProducto: {Nombre_producto}")
-        print(f"Codigo: {Datos['Codigo']}")
-        print(f"Proveedor: {Datos['Proveedor']}") 
-        for Bodega, Cantidad in Datos['Bodegas'].items():
-            print(f"Cantidad en Bodega {Bodega}: {Cantidad}")
+        print(f"Producto: {Nombre_producto}")
+        print(f"Código: {Datos['Codigo']}")
+        if 'Proveedor' in Datos:
+            print(f"Proveedor: {Datos['Proveedor']}")
         else:
-            print(f"El producto '{Nombre_producto}' No esta registrado.")
+            print("Proveedor no disponible")
+    else:
+        print(f"El producto '{Nombre_producto}' no está registrado.")
+        print("Productos disponibles en el inventario: ")
+        print(list(inventario.keys()))
