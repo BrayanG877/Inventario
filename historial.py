8bf88b90edc5aeb7185d121b04dfe6ea5fefa0e1
BrayanG877 <gordillomartinezbrayan@gmail.com>
2025-01-19 12:37:59 -0500

    Create historial.py

---
+++ historial.py
@@ -0,0 +1,8 @@
+def Historial_producto(inventario, Nombre_producto):
+    if Nombre_producto in inventario:
+        Datos = inventario[Nombre_producto]
+    print(f"\nHistorial de '{Nombre_producto}':")
+    for operacion in Datos['Historial']:
+        print(f"Fecha: {operacion['Fecha']}, Tipo:{operacion['tipo']}, Cantidad:{operacion['Cantidad']}, Bodega: {operacion['Bodega']}, Descripcion:{operacion['Descripcion']}")
+    else:
+        print(f"El producto '{Nombre_producto}' No esta registrado.")
