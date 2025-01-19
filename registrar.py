1cf6f626840811f385c2360cd264640b31cb2ee4
BrayanG877 <gordillomartinezbrayan@gmail.com>
2025-01-19 15:13:42 -0500

    Update and rename registrar to registrar.py

--- registrar.py
+++ registrar.py
@@ -3,9 +3,11 @@ def Registrar_producto(inventario, Codigo_producto,Nombre_producto,Proveedor_pro
         print(f"El Producto '{Nombre_producto}' Ya esta registrad.")
     else:
         inventario[Nombre_producto]= {
+            "Nombre": Nombre_producto,
             "Codigo" : Codigo_producto,
-            "Proveedro" : Proveedor_producto,
+            "Proveedor" : Proveedor_producto,
             "Bodegas" : {"Norte":0,"Centro":0,"Oriente":0},
             "Historial": []
         }
         print(f"Producto '{Nombre_producto}' ha sido registrado correctamente.")
+        print(inventario[Nombre_producto]) 
